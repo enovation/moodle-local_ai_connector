7b06e97fb9bd670554ded5b9db669adc800445a0
odziminski <o.dziminski@gmail.com>
2023-06-15 10:51:05 +0200

    fix 2 typos in lang strings

--- lang/en/local_ai_connector.php
+++ lang/en/local_ai_connector.php
@@ -28,11 +28,11 @@ $string['temperature'] = 'Temperature';
 $string['temperature_desc'] = 'In other words this is "randomness" or "creativity". Low temperature will generate more coherent but predictable text. The range is from 0 to 1.';
 $string['max_length'] = 'Maximum length';
 $string['top_p'] = 'Top P';
-$string['top_p_desc'] = 'It\'s used for similar purpose as temperature - the lower the setting, the more correct and deterministic output. The range is also from 0 to 1.';
+$string['top_p_desc'] = 'It\'s used for similar purpose as temperature - the lower the setting, the more correct and deterministic the output. The range is also from 0 to 1.';
 $string['frequency_penalty'] = 'Frequency penalty';
 $string['frequency_penalty_desc'] = 'Reduces repetition of words that have already been generated. It counts how many times the word was already used.';
 $string['presence_penalty'] = 'Presence penalty';
-$string['presence_penalty_desc'] = 'Similar to frequency penalty, it reduces probability of using a that was already used.
+$string['presence_penalty_desc'] = 'Similar to frequency penalty, it reduces probability of using a word that was already used.
 The difference is that is does not matter how many times the word was used - just if it was or not.';
 $string['deepaisettings'] = 'DeepAI settings';
 $string['deepaisettings_help'] = 'Settings for DeepAI (deepai.org) services (Stable Diffusion)';
